9adc0e73d0f7384bff0bb7b0f2d91601a7703f93
MMcIntosh2012 <may.mcintosh2012@gmail.com>
2014-04-24 17:00:45 -0400

    first commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,25 +1,25 @@
 #Train data frame
-setwd("C:/Users/DO TON/Downloads/Getting and Cleaning Data/Peer Assessment/UCI HAR Dataset/train/")
+directory<-"C:/Users/DO TON/Downloads/Getting and Cleaning Data/Peer Assessment/"
+setwd(paste(directory,"UCI HAR Dataset/train/",sep=""))
 x_train<-read.table("X_train.txt")
 y_train<-read.table("y_train.txt")
 subject_train<-read.table("subject_train.txt")
 df_train<-data.frame(y_train,subject_train,x_train)
 
 #Test data frame
-setwd("C:/Users/DO TON/Downloads/Getting and Cleaning Data/Peer Assessment/UCI HAR Dataset/test/")
+setwd(paste(directory,"UCI HAR Dataset/test/",sep=""))
 x_test<-read.table("X_test.txt")
 y_test<-read.table("y_test.txt")
 subject_test<-read.table("subject_test.txt")
 df_test<-data.frame(y_test,subject_test,x_test)
 
-#Merge data set
+#Merge data sets
 merged_df<-rbind(df_test,df_train)
 
 #Column name for data set
-setwd("C:/Users/DO TON/Downloads/Getting and Cleaning Data/Peer Assessment/UCI HAR Dataset")
+setwd(paste(directory,"UCI HAR Dataset/",sep=""))
 features<-read.table("features.txt")
 names<-c("Labels","Subject",as.character(features[,2]))
-
 #Rename columns of merged_df
 colnames(merged_df)<-names
 
@@ -31,21 +31,21 @@ f_vec<-sort(c(mean_vec,std_vec))
 #Extracts only the measurements on the mean and standard deviation for each measurement. 
 sub_df<-subset(merged_df,select=c(1,2,f_vec))
 
-#Read the Labels
-setwd("C:/Users/DO TON/Downloads/Getting and Cleaning Data/Peer Assessment/UCI HAR Dataset")
 #getting the label vector
 labels<-read.table("activity_labels.txt")
 #assign despcriptive labels to labels
+
 sub_df$Descriptive.Labels<-labels[,2][sub_df$Labels]
+
 #move the Descriptive.Labels ahead
 sub_df<-sub_df[,c(1,2,82,3:81)]
+
 #getting the mean for each person and for each activity
 final_df<-aggregate(sub_df,list(Subject=sub_df$Subject,Descriptive.Labels=sub_df$Descriptive.Labels),mean)
+
 #Delete the extra Subject and Descriptive.Labels column
 final_df<-final_df[,-c(4,5)]
 
 #Write to a text file
-setwd("C:/Users/DO TON/Downloads/Getting and Cleaning Data/Peer Assessment/UCI HAR Dataset")
 fileConn<-file("TidyData.txt")
 write.table(final_df,fileConn)
-
